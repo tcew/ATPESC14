310d68003256c3234f11e3ed4ca6a39232790d63
Tim Warburton <timwar@Tims-MacBook-Pro.local>
2014-07-29 11:32:20 -0500

    set up to use datafloat

--- examples/cuda/jacobi/main.cu
+++ examples/cuda/jacobi/main.cu
@@ -6,6 +6,8 @@ using namespace std;
 
 #define BDIM 256
 
+#define datafloat double
+
 /* 
 
    Poisson problem:  diff(u, x, 2) + diff(u, y, 2) = f
@@ -15,13 +17,18 @@ using namespace std;
 
    2nd order finite difference:   4*u(j,i) - u(j-1,i) - u(j+1,i) - u(j,i-1) - u(j,i+1) = -delta*delta*f(j,i) 
 
+   define: rhs(j,i) = -delta*delta*f(j,i)
+
+   Jacobi iteration: newu(j,i) = 0.25*(u(j-1,i) + u(j+1,i) + u(j,i-1) + u(j,i+1) + rhs(j,i))
+
 */
 
 __global__ void jacobi(const int N,
-                       const float *rhs,
-                       const float *u,
-                       float *newu){
-  // Get thread ID
+                       const datafloat *rhs,
+                       const datafloat *u,
+                       datafloat *newu){
+
+  // Get thread indices
   const int i = blockIdx.x*blockDim.x + threadIdx.x;
   const int j = blockIdx.y*blockDim.y + threadIdx.y;
 
@@ -30,7 +37,7 @@ __global__ void jacobi(const int N,
     // Get padded grid ID
     const int pid = (j + 1)*(N + 2) + (i + 1);
 
-    float invD = 0.25;
+    datafloat invD = 0.25;
 
     newu[pid] = invD*(rhs[pid]
 		      + u[pid - (N+2)]
@@ -41,9 +48,9 @@ __global__ void jacobi(const int N,
 }
 
 __global__ void partialReduceResidual(const int entries,
-                               float *u,
-                               float *newu,
-                               float *red){
+                               datafloat *u,
+                               datafloat *newu,
+                               datafloat *red){
   __shared__ double s_red[BDIM];
 
   const int id = blockIdx.x*blockDim.x + threadIdx.x;
@@ -51,7 +58,7 @@ __global__ void partialReduceResidual(const int entries,
   s_red[threadIdx.x] = 0;
 
   if(id < entries){
-    const float diff = u[id] - newu[id];
+    const datafloat diff = u[id] - newu[id];
     s_red[threadIdx.x] = diff*diff;
   }
 
@@ -125,8 +132,8 @@ int main(int argc, char** argv){
   }
 
   const int N     = atoi(argv[1]);
-  const float tol = atof(argv[2]);
-  float res = 0;
+  const datafloat tol = atof(argv[2]);
+  datafloat res = 0;
 
   const int iterationChunk = 100; // needs to be multiple of 2
   int iterationsTaken = 0;
@@ -140,18 +147,18 @@ int main(int argc, char** argv){
   dim3 rGrid((N*N + rBlock.x - 1)/rBlock.x);
 
   // Host Arrays
-  float *h_u   = (float*) calloc((N+2)*(N+2), sizeof(float));
-  float *h_u2  = (float*) calloc((N+2)*(N+2), sizeof(float));
-  float *h_rhs = (float*) calloc((N+2)*(N+2), sizeof(float));
+  datafloat *h_u   = (datafloat*) calloc((N+2)*(N+2), sizeof(datafloat));
+  datafloat *h_u2  = (datafloat*) calloc((N+2)*(N+2), sizeof(datafloat));
+  datafloat *h_rhs = (datafloat*) calloc((N+2)*(N+2), sizeof(datafloat));
 
-  float *h_res = (float*) calloc(rGrid.x, sizeof(float));
+  datafloat *h_res = (datafloat*) calloc(rGrid.x, sizeof(datafloat));
 
-  float delta = 2./(N+1);
-  float normRHS = 0;
+  datafloat delta = 2./(N+1);
+  datafloat normRHS = 0;
   for(int j = 0; j < N+2; ++j){
     for(int i = 0; i < N+2; ++i){
-      float x = -1 + delta*i;
-      float y = -1 + delta*j;
+      datafloat x = -1 + delta*i;
+      datafloat y = -1 + delta*j;
 
       h_rhs[i + (N+2)*j] = delta*delta*(2.*M_PI*M_PI*sin(M_PI*x)*sin(M_PI*y));
       normRHS += pow(h_rhs[i+(N+2)*j],2);
@@ -160,18 +167,18 @@ int main(int argc, char** argv){
   normRHS = sqrt(normRHS);
   
   // Device Arrays
-  float *c_u, *c_u2, *c_rhs, *c_res;
+  datafloat *c_u, *c_u2, *c_rhs, *c_res;
 
-  cudaMalloc((void**) &c_u  , (N+2)*(N+2)*sizeof(float));
-  cudaMalloc((void**) &c_u2 , (N+2)*(N+2)*sizeof(float));
-  cudaMalloc((void**) &c_rhs ,(N+2)*(N+2)*sizeof(float));
+  cudaMalloc((void**) &c_u  , (N+2)*(N+2)*sizeof(datafloat));
+  cudaMalloc((void**) &c_u2 , (N+2)*(N+2)*sizeof(datafloat));
+  cudaMalloc((void**) &c_rhs ,(N+2)*(N+2)*sizeof(datafloat));
 
-  cudaMalloc((void**) &c_res, rGrid.x*sizeof(float));
+  cudaMalloc((void**) &c_res, rGrid.x*sizeof(datafloat));
 
   // Setting device vectors to 0
-  cudaMemcpy(c_u ,  h_u ,  (N+2)*(N+2)*sizeof(float), cudaMemcpyHostToDevice);
-  cudaMemcpy(c_u2,  h_u2,  (N+2)*(N+2)*sizeof(float), cudaMemcpyHostToDevice);
-  cudaMemcpy(c_rhs, h_rhs, (N+2)*(N+2)*sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(c_u ,  h_u ,  (N+2)*(N+2)*sizeof(datafloat), cudaMemcpyHostToDevice);
+  cudaMemcpy(c_u2,  h_u2,  (N+2)*(N+2)*sizeof(datafloat), cudaMemcpyHostToDevice);
+  cudaMemcpy(c_rhs, h_rhs, (N+2)*(N+2)*sizeof(datafloat), cudaMemcpyHostToDevice);
 
   // Create CUDA events
   cudaEvent_t startEvent, endEvent;
@@ -194,7 +201,7 @@ int main(int argc, char** argv){
     partialReduceResidual<<<rGrid, rBlock>>>(N*N, c_u, c_u2, c_res);
 
     // Finish reduce in host
-    cudaMemcpy(h_res, c_res, rGrid.x*sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(h_res, c_res, rGrid.x*sizeof(datafloat), cudaMemcpyDeviceToHost);
 
     res = 0;
     for(int i = 0; i < rGrid.x; ++i)
@@ -216,29 +223,29 @@ int main(int argc, char** argv){
   cudaEventElapsedTime(&timeTaken, startEvent, endEvent);
 
   // Copy final solution from device array to host
-  cudaMemcpy(h_u, c_u, (N+2)*(N+2)*sizeof(float), cudaMemcpyDeviceToHost);
+  cudaMemcpy(h_u, c_u, (N+2)*(N+2)*sizeof(datafloat), cudaMemcpyDeviceToHost);
 
   // Compute maximum error in finite difference solution
   FILE *fp = fopen("result.dat", "w");
-  float maxError = 0;
+  datafloat maxError = 0;
   for(int j = 0; j < N+2; ++j){
     for(int i = 0; i < N+2; ++i){
-      float x = -1 + delta*i;
-      float y = -1 + delta*j;
-      float error = fabs( sin(M_PI*x)*sin(M_PI*y) - h_u[i + (N+2)*j]);
+      datafloat x = -1 + delta*i;
+      datafloat y = -1 + delta*j;
+      datafloat error = fabs( sin(M_PI*x)*sin(M_PI*y) - h_u[i + (N+2)*j]);
       maxError = (error > maxError) ? error:maxError;
       fprintf(fp, "%g %g %g %g\n", x, y, h_u[i+(N+2)*j],error);
     }
   }
   fclose(fp);
 
-  const float avgTimePerIteration = timeTaken/((float) iterationsTaken);
+  const datafloat avgTimePerIteration = timeTaken/((datafloat) iterationsTaken);
 
   printf("Top right current          : %7.9e\n"     , h_u[N*(N+2) + N]);
   printf("Residual                   : %7.9e\n"     , res);
   printf("Iterations                 : %d\n"        , iterationsTaken);
   printf("Average time per iteration : %3.5e ms\n"  , avgTimePerIteration);
-  printf("Bandwidth                  : %3.5e GB/s\n", (1.0e-6)*(6*N*N*sizeof(float))/avgTimePerIteration);
+  printf("Bandwidth                  : %3.5e GB/s\n", (1.0e-6)*(6*N*N*sizeof(datafloat))/avgTimePerIteration);
   printf("Maximum absolute error     : %7.9e\n"     ,  maxError);
 
   // Free all the mess
