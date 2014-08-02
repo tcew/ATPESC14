039a1a84a36cc746079fc2918f5776645b072ca3
Tim Warburton <timwar@Tims-MacBook-Pro.local>
2014-08-01 20:40:34 -0500

    changed names

--- examples/cuda/jacobi/main.cu
+++ examples/cuda/jacobi/main.cu
@@ -101,6 +101,40 @@ __global__ void jacobiShared(const int N,
 
 
 __global__ void partialReduceResidual(const int entries,
+				      datafloat *u,
+				      datafloat *newu,
+				      datafloat *blocksum){
+
+  __shared__ datafloat s_blocksum[BDIM];
+
+  const int id = blockIdx.x*blockDim.x + threadIdx.x;
+
+  s_blocksum[threadIdx.x] = 0;
+
+  if(id < entries){
+    const datafloat diff = u[id] - newu[id];
+    s_blocksum[threadIdx.x] = diff*diff;
+  }
+
+  int alive = blockDim.x;
+  int t = threadIdx.x;
+
+  while(alive>1){
+
+    __syncthreads();  // barrier (make sure s_red is ready)
+    
+    alive /= 2;
+    if(t < alive)
+      s_blocksum[t] += s_blocksum[t+alive];
+  }
+  
+  if(t==0) 
+    blocksum[blockIdx.x] = s_blocksum[0];
+}
+  
+
+
+__global__ void unrolledPartialReduceResidual(const int entries,
                                datafloat *u,
                                datafloat *newu,
                                datafloat *red){
